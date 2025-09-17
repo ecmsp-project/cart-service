f544e10cc9bb55ba39d07d3fa1251c11eebeeecd
Wojciech Orłowski <vemtorpl@gmail.com>
2025-09-17 20:03:42 +0200

    adding grpc reservation request logic (#9)

--- src/main/java/com/ecmsp/cartservice/dto/reservation/ReservationFailedResponse.java
+++ src/main/java/com/ecmsp/cartservice/dto/reservation/ReservationFailedResponse.java
@@ -1,4 +1,14 @@
 package com.ecmsp.cartservice.dto.reservation;
 
+import java.util.List;
+
 public class ReservationFailedResponse extends ReservationResponse {
+
+    public ReservationFailedResponse(String message) {
+        super(false, message, List.of());
+    }
+
+    public ReservationFailedResponse() {
+        super(false, "Product reservation failed. Please try again or contact support.", List.of());
+    }
 }
